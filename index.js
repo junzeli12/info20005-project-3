611c585c0be8a0c121f98d0d6fe254ae0543f4d7
junzeli12 <junzel@student.unimelb.edu.au>
2022-06-09 16:47:27 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var btn=document.getElementById("btn").getElementsByTagName("button")[0];
+    btn.onclick = function(){
+        location.href = 'search page.html';
+    }
+}
+     /*when user click the search button, it can be link the search page*/
